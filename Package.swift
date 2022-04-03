32826ed840887a4edd3e07bf78f05196ec43dfe9
Kevin Renskers <kevin@loopwerk.io>
2022-04-03 11:13:59 +0200

    fix: Remove the PathKit dependency, as that is already included with Saga

--- Package.swift
+++ Package.swift
@@ -13,14 +13,13 @@ let package = Package(
       targets: ["SagaStencilRenderer"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/loopwerk/PathKit", from: "1.2.0"),
     .package(url: "https://github.com/loopwerk/Saga", from: "1.0.0"),
     .package(url: "https://github.com/stencilproject/Stencil", from: "0.14.0"),
   ],
   targets: [
     .target(
       name: "SagaStencilRenderer",
-      dependencies: ["PathKit", "Saga", "Stencil"]
+      dependencies: ["Saga", "Stencil"]
     ),
   ]
 )
